757c8fe2ecf1d52a608c38a55894a1972cc9ccc8
don2001 <dolethamma@gmail.com>
2023-06-07 13:55:10 +0100

    done

---
+++ Z.py
@@ -0,0 +1,18 @@
+'''
+num1, num2 = 5, 15
+ans = num1+num2
+
+print(f"The addition of {num1} and {num2} results {ans}.")
+'''
+'''
+f = open("input.txt", "r")
+g=open("output.txt","w")
+x=f.readline()
+lines = f.read().split(" ")
+for i in range(len(lines)):
+   g.write(lines[i]+'\n')
+g.close()
+'''
+
+print(998244353**2)
+
